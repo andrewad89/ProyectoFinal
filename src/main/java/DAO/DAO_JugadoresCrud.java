028f289fb19966991d0f93e14a96d4e74de6efe1
PedroDAW <shinja77@hotmail.com>
2015-05-28 00:58:18 +0200

    metodos delete y update

--- src/main/java/DAO/DAO_JugadoresCrud.java
+++ src/main/java/DAO/DAO_JugadoresCrud.java
@@ -6,6 +6,7 @@
 package DAO;
 
 import Entidad.Jugador;
+import daw.ed.spark.JugadoresCrud;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
